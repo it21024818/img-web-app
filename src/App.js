cc6a40f945edcb6ecb97811dc7e8003bd722166a
Dinuka Dissanayake <90415@cmg.lk>
2023-08-08 01:06:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import Webcam from 'react-webcam';
 
 function App() {
+  const [language, setLanguage] = useState("");
+  const [picture, setPicture] = useState("");
+  const webcamRef = useRef(null);
+  const [start, setStart] = useState(false);
+  const [audioSrc, setAudioSrc] = useState("");
+
+  useEffect(() => {
+    if (start) {
+      const capture = setInterval(() => {
+        setPicture(webcamRef.current.getScreenshot());
+      }, 5000);
+
+      return () => clearInterval(capture);
+    }
+  }, [start]);
+
+  useEffect(() => {
+    if (picture) {
+      const base64Image = picture.split(',')[1];
+
+      axios.post('http://localhost:8000/img/', {
+        image: base64Image,
+        language: language,
+      }).then(response => {
+        setAudioSrc(response.data.output);
+        const audio = new Audio(audioSrc);
+        audio.play();
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  }, [picture, language]);
+
+  const handleStart = () => {
+    setStart(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Webcam audio={false} screenshotFormat="image/jpeg" ref={webcamRef} />
+      <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+        <option value="">Select a language</option>
+        <option value="en">English</option>
+        <option value="fr">French</option>
+        {/* Add more language options here */}
+      </select>
+      <button onClick={handleStart}>Start</button>
     </div>
   );
 }
